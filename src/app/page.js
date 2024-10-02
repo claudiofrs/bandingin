34ebcb869ff05cacbd47641a74587c169a9fc614
claudiofresta <claudiofresta@gmail.com>
2024-10-02 21:51:11 +0700

    add new paragraph test

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ export default function Home() {
     <main className="max-w-md mx-auto py-10 flex flex-col space-y-4 px-3 sm:px-0">
       <h1 className="text-xl text-center text-green-500 font-medium">Ban Dingin</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nobis assumenda iure sit deserunt quod culpa illo, iste velit quo dignissimos, possimus voluptatibus at perspiciatis qui modi maxime non? Perferendis!</p>
+      <p>testing new branch</p>
     </main>
   );
 }
